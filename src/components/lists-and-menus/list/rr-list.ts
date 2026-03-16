d3ea4a125720e2710bcf3f222c6c88f756878801
Bart van de Biezen <bart@vandebiezen.com>
2026-03-16 12:23:09 +0100

    fix(rr-list): replace shadowRoot! non-null assertion with this.renderRoot for safer clone append

--- src/components/lists-and-menus/list/rr-list.ts
+++ src/components/lists-and-menus/list/rr-list.ts
@@ -262,7 +262,7 @@ export class RRList extends LitElement {
 			this._clone.style.setProperty('--_drag-clone-width', `${rect.width}px`);
 			this._clone.style.setProperty('--_drag-clone-height', `${rect.height}px`);
 			this._clone.appendChild(hostClone);
-			this.shadowRoot!.appendChild(this._clone);
+			this.renderRoot.appendChild(this._clone);
 		}
 	}
 
