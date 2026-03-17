e6561a8d9b85e1cf8abd1c1fb986a3c7b0dcb478
Bart van de Biezen <bart@vandebiezen.com>
2026-03-17 16:12:59 +0100

    refactor(inputs): refactor rr-search-field, add dismiss button, search button and i18n

---
+++ src/components/inputs/search-field/rr-search-field.i18n.ts
@@ -0,0 +1,6 @@
+export const rrSearchFieldTranslations = {
+	'components.search-field.dismiss-action': 'Wis zoekopdracht',
+	'components.search-field.search-action': 'Zoek',
+};
+
+export type RRSearchFieldTranslations = typeof rrSearchFieldTranslations;
